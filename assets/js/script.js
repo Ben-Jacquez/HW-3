c9007e0c9b15cfd1a6641c1e84f8ced3210c88c5
Ben Jacquez <ben@Bens-MBP.attlocal.net>
2022-12-02 22:03:26 -0800

    refactored and debugged

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,10 @@
-const generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // create variables to hold the options we need: lowercase, uppercase, numeric and special characters
-const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '?'];
-
-// create an empty array that will hold the users selected characters
-// var possibleCharacters = []
-// var passwordArray = []
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '?'];
 
 // upon start prompt the users number of characters, then hold that in a variable
 function passwordOptions() {
@@ -17,33 +13,31 @@ function passwordOptions() {
   );
 
 // check the users input and confirm they put in a numeric value that is between 8 and 128, & write a function that validates the user input
-  if (characters >= 8 && characters <= 128) {
-    let passwordLowercase = confirm("Would you like to include lowercase letters?");
-    let passwordUppercase = confirm("Would you like to include uppercase letters?");
-    let passwordNumbers = confirm("Would you like to include numbers?");
-    let passwordSpecial = confirm("Would you like to include special characters?");
-    if (!passwordLowercase && !passwordUppercase && !passwordNumbers && !passwordNumbers) {
-    alert("You must select at least one type of character.");
-    }
-// after validation create variables for character selections
-    let options = {
-      strength: characters,
-      passwordLowercase: passwordLowercase,
-      passwordUppercase: passwordUppercase,
-      passwordNumbers: passwordNumbers,
-      passwordSpecial: passwordSpecial,
-    };
-    return options;
-
-    } else {
-      alert("Must be between 8 and 128 characters")
-    }
-  
+if (characters >= 8 && characters <= 128) {
+  var passwordLowercase = confirm("Would you like to include lowercase letters?");
+  var passwordUppercase = confirm("Would you like to include uppercase letters?");
+  var passwordNumbers = confirm("Would you like to include numbers?");
+  var passwordSpecial = confirm("Would you like to include special characters?");
+// Confirm the user selected at least one character option for the password
+  if (!passwordLowercase && !passwordUppercase && !passwordNumbers && !passwordSpecial) {
+  alert("You must select at least one type of character.");
 }
-// after validation is complete start pushing the selected characters into possible chars array
-// after validation create variables for character selections
+  let options = {
+    strength: characters,
+    passwordLowercase: passwordLowercase,
+    passwordUppercase: passwordUppercase,
+    passwordNumbers: passwordNumbers,
+    passwordSpecial: passwordSpecial,
+  };
+  return options;
+
+} else {
+  alert("Must be between 8 and 128 characters")
+}
+}
+  
 function generatePassword() {
-  console.log("test");
+// after validation create variables for character selections
   let userPasswordChoices = passwordOptions();
   let userChoice = "";
   let password = "";
@@ -59,49 +53,26 @@ function generatePassword() {
     userChoice += numbers.join("");
   }
   if (userPasswordChoices.passwordSpecial) {
-  userChoice =+ specials.join("");
+ userChoice += specials.join("");
   }
 
-//  For (let i = 0; i < userPasswordChoices.strength; i++) {
-//  let random = math.floor(math.random() * userChoice.length);
-//  password += userChoice.charAt(random);
-//}
-
-//return generatePassword()  
+// loop through passwordCharacters and select a random character from the possibleCharacters array based on how many characters they entered into the prompt
+  for (let i = 0; i < userPasswordChoices.strength; i++) {
+    let random = Math.floor(Math.random() * userChoice.length);
+    password += userChoice.charAt(random);
+  }
   return password;
 }
-
+  
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
+  var finalPassword = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = finalPassword;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-//}
-
-
-//if (passwordLowercase) {
-// passwordCharacters.contact(passwordLowercase)
-//}
-//if (passwordUppercase) {
-// passwordCharacters.contact(passwordUppercase);
-//}
-//if (passwordNumbers) {
-//  passwordCharacters.contact(passwordNumbers);
-//}
-//if (passwordSpecial) {
-//  passwordCharacters.contact(passwordSpecial);
-//}
-
-// create a random number
-
-// loop through passwordCharacters and select a random character from the possibleCharacters array based on how many characters they entered into the prompt
-
-// push() the randomly generated character to an empty array called passwordArray
-
-// return passwordArray.join()
